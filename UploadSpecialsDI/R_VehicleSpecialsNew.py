e5634ff9b3d469b9dbc536110a33fe2c91a8fd95
Matthew Muhlenkort <mmuhlenkort@walser.com>
2020-04-14 17:51:10 -0500

    Added more functions to VehicleSpecialsNew

--- UploadSpecialsDI/R_VehicleSpecialsNew.py
+++ UploadSpecialsDI/R_VehicleSpecialsNew.py
@@ -65,11 +65,113 @@ class VehicleSpecialsNew():
                     assert driver.switch_to.alert.text == "Vehicle Stock or VIN not found"
                 continue
 
-    def build_special(v, driver):
-        refresh_post_page(v,driver)
-        populate_vehicle(v, driver)
-        populate_title_boxes(v, driver)
+    def use_advanced_options_tab(v, driver):
+        while True: # Advanced Options Tab
+            try:
+                driver.find_element(By.LINK_TEXT, "Advanced Options").click()
+                driver.find_element(By.LINK_TEXT, "Add Discount").click()
+                driver.find_element(By.LINK_TEXT, "Add Discount").click()
+                offer_input = driver.find_element_by_xpath(self.offer_path(Region, 1, 2, vehicle))
+                offer_input_css = offer_input.find_element_by_css_selector('input')
+                offer_type_input = driver.find_element_by_xpath(self.offer_path(Region, 1, 3, vehicle))
+                offer_type_input_css = offer_type_input.find_element_by_css_selector('input')
+                for advanced_options in vehicle.Offers:
+                    if '10% Down Lease Special' in advanced_options.LeaseSpecial or 'OEM' in advanced_options.LeaseSpecial:
+                        driver.execute_script(f'arguments[0].value = "{advanced_options.LeaseOffer}";', offer_input_css)
+                        special = f'\<br\>{advanced_options.LeaseSpecial}\<br\>{advanced_options.DueAtSigning} Due at Signing'
+                        driver.execute_script(f'arguments[0].value = "{special}";', offer_type_input_css)
+                
+                offer_input = driver.find_element_by_xpath(self.offer_path(Region, 2, 2, vehicle))
+                offer_input_css = offer_input.find_element_by_css_selector('input')
+                driver.execute_script('arguments[0].value = "CLICK HERE FOR MORE OFFERS";', offer_input_css)
+                value_list = []
+                element = driver.find_element_by_xpath('//*[@id="typediv"]/button/span[2]')
+                actions = ActionChains(self.driver)
+                actions.move_to_element(element).perform()
+                print('found element')
+                domain = website
+                if domain == 'https://www.walserautocampus.com/':
+                    print('campus')
+                    value_list = [
+                    f'{dictionary[f"{vehicle.DealerCode} {vehicle.MakeName}"]["OfferType WAC"]}',
+                    'in-type-94', 'in-type-95', 'in-type-58' ]
+                    print(value_list)
+                    self.offer_type_clicks(value_list)
+                    break
+                elif domain == 'https://www.walser.com/':
+                    print('not campus')
+                    value_list = [ 
+                        f'in-type-33',
+                        f'{dictionary[f"{vehicle.DealerCode} {vehicle.MakeName}"]["OfferType StateCode"]}',
+                        f'{dictionary[f"{vehicle.DealerCode} {vehicle.MakeName}"]["OfferType New"]}',
+                        f'{dictionary[f"{vehicle.DealerCode} {vehicle.MakeName}"]["OfferType Walser"]}'
+                        ]
+                    print(value_list)
+                    self.offer_type_clicks(value_list)
+                    break
+                else:
+                    if domain == 'https://www.walsernissan.com/':
+                        v.DealerCode = 'NIS'
+                    if domain == 'https://www.walsernissancoonrapids.com/':
+                        v.DealerCode = 'CRN'
+                    if domain == 'https://www.walsernissanwayzata.com/':
+                        v.DealerCode = 'WZMNNS'
+                    if domain == 'https://www.walser-mazda.com/':
+                        v.DealerCode = 'MAZ'
+                    if domain == 'https://www.walserpolarmazda.com/':
+                        v.DealerCode = 'WBMNMA'
+                    print(f"{vehicle.DealerCode} {vehicle.MakeName}")
+                    value_list = [
+                        
+                        f'{dictionary[f"{vehicle.DealerCode} {vehicle.MakeName}"]["OfferType Store"]}'
+                    ]
+                    print(value_list)
+                    self.offer_type_clicks(value_list)
+                    break
+            except:
+                assert driver.switch_to.alert.text == "Vehicle Stock or VIN not found"
+                continue 
 
+    def use_offer_tab(v, driver):
+        while True: # Offer Tab
+            try:
+                driver.find_element(By.LINK_TEXT, "Offer").click()# Click OFFER Tab 
+                driver.find_element(By.ID, "acf-field_569c2e95a3d27").send_keys("View Vehicle Details")# Primary Button Label
+                v = f'{vehicle.Year} {vehicle.MakeName} {vehicle.ModelName}'.replace(' ','-')
+                x = f'/new-vehicles/#action=im_ajax_call&perform=get_results&search={v}&page=1'
+                z = driver.find_element(By.ID, "acf-field_569c2aa7a3d24")#Secondary Button Link
+                driver.execute_script(f'arguments[0].value = "{x}";', z)
+                driver.find_element(By.ID, "acf-field_569c2bb9a3d26").send_keys("View Inventory")#Secondary Link Label
+                
+                self.offer_labels(vehicle)
+                
+                self.offer_description(vehicle, Region)
+                #self.driver.find_element(By.ID, "acf-field_5577c0782430f").send_keys("Offer Disclaimer")
+                
+                self.offer_disclaimer(vehicle)
+                break
+            except:
+                assert driver.switch_to.alert.text == "Vehicle Stock or VIN not found"
+                continue
+
+    def populate_special():
+        while True:
+            try:
+                element = driver.find_element(By.ID, "title")
+                actions = ActionChains(self.driver)
+                actions.move_to_element(element).perform()
+                driver.find_element(By.ID, "publish").click()
+                break
+            except:
+                print('Failed')
+                sleep(.1)
+
+    def build_special(v, driver):
+        Today.time_taken(refresh_post_page, v, driver)
+        Today.time_taken(populate_vehicle, v, driver)
+        Today.time_taken(populate_title_boxes, v, driver)
+        Today.time_taken(use_advanced_options_tab, v, driver)
+        Today.time_taken(use_offer_tab, v, driver)
 
     def check_match(v):
         value = None
@@ -104,13 +206,13 @@ class VehicleSpecialsNew():
                         continue
                     elif w.Domain == 'https://www.walser.com/' or w.Domain == 'https://www.walserautocampus.com/':
                         print(f'CHECK BLOCK 4 TRUE: Running Specials on {w.Domain}')
-                        build_special(v, driver)
+                        Today.time_taken(build_special, v, driver)
                     elif v.Brand != w.Brand:
                         print(f'CHECK BLOCK 5 TRUE: Vehicle Brand: {v.Brand} \nWebsite Brand: {w.Brand}')
                         continue
                     else:
                         print(f'ELSE STATMENT ACTIVE: Running Specials on {w.Domain}')
-                        build_special(v, driver)
+                        Today.time_taken(build_special, v, driver)
 
             else:
                 pass
